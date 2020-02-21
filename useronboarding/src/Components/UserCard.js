71c0ccd032ef6910e4179c5f7ada465c3a737d92
rabah babaci <4rabah@gmail.com>
2020-02-20 22:29:19 -0800

    map over the users data and render a test user

--- useronboarding/src/Components/UserCard.js
+++ useronboarding/src/Components/UserCard.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default function UserCard() {
-  return <div></div>;
+export default function UserCard({ users }) {
+  return (
+    <div className="users-wrapper">
+      {users.map(user => (
+        <div>
+          <h3>
+            First Name: <span>{user.firstName}</span>
+          </h3>
+          <h3>
+            First Name: <span>{user.lastName}</span>
+          </h3>
+          <h3>
+            First Name: <span>{user.email}</span>
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
 }
