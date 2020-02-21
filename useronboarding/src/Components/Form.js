42d57e18d2bd7a5742dae6ef289a79874be24cb9
rabah babaci <4rabah@gmail.com>
2020-02-20 17:40:34 -0800

    create basic form inputs

--- useronboarding/src/Components/Form.js
+++ useronboarding/src/Components/Form.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Form, Field, withFormik } from "formik";
 
-function InputForm() {
+function InputForm(props) {
   return (
     <div>
       <Form>
         <label>
           <Field type="text" name="name" placeholder="Name" />
         </label>
+
+        <label>
+          <Field type="email" name="email" placeholder="Email" />
+        </label>
+
+        <label>
+          <Field type="password" name="password" placeholder="Password" />
+        </label>
+
+        <label>
+          <Field type="checkbox" name="serviceTerms" />
+        </label>
+
+        <button type="submit">Submit</button>
       </Form>
     </div>
   );
